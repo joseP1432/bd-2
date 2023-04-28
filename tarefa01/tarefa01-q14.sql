df4048cb03446770b5782a5641116724bd42fc31
José Marques <jose.marques15@hotmail.com>
2023-04-27 21:38:20 -0300

    reorganização das pastas e arquivos #1

--- tarefa01/tarefa01-q14.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW total_atv AS
-SELECT cod_responsavel, COUNT(*) as TOTAL
-FROM atividade
-GROUP BY cod_responsavel;
-
-SELECT f.nome, ta.TOTAL
-FROM funcionario f, total_atv ta 
-WHERE f.codigo = ta.cod_responsavel;
